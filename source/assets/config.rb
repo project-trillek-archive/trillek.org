3aeb78b70e3a2be314bde3f11d1008b2d88f0c2b
Oliver Caldwell <olliec87@gmail.com>
2013-08-22 14:01:01 +0100

    Configured compass a bit.

--- source/assets/config.rb
+++ source/assets/config.rb
@@ -1,11 +1,13 @@
 # Require any additional compass plugins here.
+require "normalize"
+require "susy"
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
+http_path = "/assets"
+css_dir = "css"
+sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
